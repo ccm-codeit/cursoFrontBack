b8510b6b5e4f97c53ad6709ab15000300e1ab954
Eric Chao <ericjardon@hotmail.com>
2021-03-21 17:23:16 -0600

    Save commit

--- src/App.js
+++ src/App.js
@@ -4,33 +4,32 @@ import Header from "./components/Header";
 import { useState } from "react";
 //TEST
 function App() {
-  const [card, setCard] = useState({
-    // la información de los inputs
-    title: "",
-    description: "",
-  });
+  
+  const [tweet, setTweet] = useState("");
 
-  const [cards, setCards] = useState([]); // lista de nuestras 'Opinion Cards'
+  const [tweetList, setTweetList] = useState([]); // lista de nuestras 'Opinion Cards'
 
   const handleOnChange = (e) => {
     // manejador de cambios en input
-    setCard({
+    /*setCard({
       ...card,
       [e.target.name]: e.target.value,
-    });
+    });*/
   };
 
   const handleOnDelete = (index) => {
+    /*
     cards.filter(() => {
       return setCards(cards.filter((card, i) => i !== index));
     });
+    */
   };
 
   const onSubmit = (e) => {
     // agrega una nueva card a la lista de cards
     e.preventDefault(); // evita que recargue la página
-    setCards((cards) => [...cards, card]);
-    console.log(cards);
+    //setCards((cards) => [...cards, card]);
+    console.log(tweetList);
   };
 
   return (
