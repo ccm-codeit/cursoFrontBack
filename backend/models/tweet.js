31e1be3c4fa116db3bbbbdcf0e3a2c2e5ad8d8b5
einar <einarlop0@gmail.com>
2021-03-27 14:12:51 -0600

    Complete begin branch

--- backend/models/tweet.js
+++ backend/models/tweet.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Crear un esquema de los datos a guardar
 const tweetSchema = new mongoose.Schema({
-    username: String,
-    tweet: String,
-    posted: {type: Date, default: Date.now()},
-})
+  username: String,
+  tweet: String,
+  posted: { type: Date, default: Date.now() },
+});
 
 // Exportar un modelo utilizable del esquema
-module.exports = mongoose.model('Tweet', tweetSchema, 'tweets');
+module.exports = mongoose.model("Tweet", tweetSchema, "Tweets");
